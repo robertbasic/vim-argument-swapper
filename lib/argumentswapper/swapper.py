db0e2d77aaa76f72eecf408823b965b4217bfc77
Robert Basic <robertbasic.com@gmail.com>
2017-01-11 20:39:35 +0100

    Find complete first variable

--- lib/argumentswapper/swapper.py
+++ lib/argumentswapper/swapper.py
@@ -1,3 +1,20 @@
 def swap(first_variable, line, column):
     swapped_line = line
+    complete_first_variable = find_first_variable(line, column)
     return swapped_line
+
+def find_first_variable(line, column):
+    line_until_column = line[:column]
+    line_from_column = line[column:]
+
+    start_column = line_until_column.rfind(",")
+    if start_column == -1:
+        start_column = line_until_column.rfind("(")
+    start_column = start_column + 1
+
+    end_column = line_from_column.find(",")
+    if end_column == -1:
+        raise IndexError
+    end_column = column + end_column
+
+    return line[start_column:end_column]
