bb3527928fa0a6a4ff0b567ae45287b16c6819dd
Robert Basic <robertbasic.com@gmail.com>
2017-01-12 09:07:33 +0100

    Test a bit more complicated examples

--- lib/argumentswapper/test_swapper.py
+++ lib/argumentswapper/test_swapper.py
@@ -7,11 +7,35 @@ class SwapperTestCase(unittest.TestCase):
         first_variable = "haystack"
         #       012345678901234567890123456789
         line = "if (in_array($haystack, $a)) {"
-        # cursor wasy on "y" in "haystack"
+        # cursor was on "y" in "haystack"
         column = 16
         result = swap(first_variable, line, column)
 
         expected = "if (in_array($a, $haystack)) {"
+        self.assertEqual(expected, result)
+
+    def test_swapping_complex(self):
+        first_variable = "array"
+        # The \\ is an escaped \
+        #       012345678901234567890123456789012345678901234567890123 456789012345678901
+        line = 'public function __construct(array $an_array = [], Some\\Namespace $order)'
+        # cursor was on "r" of "array" type hint
+        column = 30
+
+        result = swap(first_variable, line, column)
+
+        expected = 'public function __construct(Some\\Namespace $order, array $an_array = [])'
+        self.assertEqual(expected, result)
+
+    def test_swapping_with_multiple_variables(self):
+        first_variable = "b"
+        #       0123456789091234567890
+        line = "$x = func($b, $a, $c);"
+        column = 10
+
+        result = swap(first_variable, line, column)
+
+        expected = "$x = func($a, $b, $c);"
         self.assertEqual(expected, result)
 
 if __name__ == '__main__':
